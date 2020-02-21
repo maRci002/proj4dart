377262818cdaf4e61b7143988ae5960b0fad6cc8
Gergely Padányi-Gulyás <fegyi001@gmail.com>
2020-02-21 21:23:07 +0100

    example: back to original projection

--- example/proj4dart_example.dart
+++ example/proj4dart_example.dart
@@ -3,6 +3,7 @@ import 'package:proj4dart/src/proj_store.dart';
 
 void main() {
   ProjStore().start();
+
   ProjDefStore().register('EPSG:23700',
       '+proj=somerc +lat_0=47.14439372222222 +lon_0=19.04857177777778 +k_0=0.99993 +x_0=650000 +y_0=200000 +ellps=GRS67 +towgs84=52.17,-71.82,-14.9,0,0,0,0 +units=m +no_defs');
 
@@ -10,13 +11,15 @@ void main() {
   var epsg4326 = Projection('EPSG:4326');
   var epsg3857 = Projection('EPSG:3857');
 
-  var point =
-      epsg23700.transform(epsg4326, Point.fromArray([649706.59, 241212.75]));
+  var pointEpsg23700 = Point.fromArray([649706.59, 241212.75]);
+  var point = epsg23700.transform(epsg4326, pointEpsg23700);
+  var point2 = epsg23700.transform(epsg3857, pointEpsg23700);
+  var point3 = epsg4326.transform(epsg23700, point);
+  var point4 = epsg3857.transform(epsg23700, point2);
 
-  print(point);
-
-  var point2 =
-      epsg23700.transform(epsg3857, Point.fromArray([649706.59, 241212.75]));
-
-  print(point2);
+  print('EPSG:23700: $pointEpsg23700');
+  print('EPSG:23700 -> EPSG:4326: $point');
+  print('EPSG:23700 -> EPSG:3857: $point2');
+  print('EPSG:23700 -> EPSG:4326 -> EPSG:23700: $point3');
+  print('EPSG:23700 -> EPSG:3857 -> EPSG:23700: $point4');
 }
