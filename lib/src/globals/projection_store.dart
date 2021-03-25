7002ece4b43e92625e2c61fe3233f8f569d29ea1
maRci002 <matuzmarci@gmail.com>
2021-03-25 14:54:40 +0100

    add meta nullsafe dependency

--- lib/src/globals/projection_store.dart
+++ lib/src/globals/projection_store.dart
@@ -14,7 +14,7 @@ class ProjectionStore {
 
   // Key: Cordinate System / Country / Code / !Any alias!
   // Value: Projection
-  final Map<String, Projection > _projectionCache;
+  final Map<String, Projection> _projectionCache;
 
   bool get isEmpty => _projectionCache.isEmpty;
 
@@ -33,9 +33,11 @@ class ProjectionStore {
   }
 
   /// Private constructor
-  ProjectionStore._internalInitializer(
-      {required Projection wgs84, required Projection nad83, required Projection google})
-      : WGS84 = wgs84,
+  ProjectionStore._internalInitializer({
+    required Projection wgs84,
+    required Projection nad83,
+    required Projection google,
+  })   : WGS84 = wgs84,
         GOOGLE = google,
         NAD83 = nad83,
         _projectionCache = {} {
